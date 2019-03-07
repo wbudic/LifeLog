1a986001d448639ffe1d910f97814126572f24db
wbudic <vilijanac@gmail.com>
2019-03-08 08:46:52 +1100

    Service enabled and instructions updated.

--- startDevWebServer.sh
+++ startDevWebServer.sh
@@ -1,4 +1,5 @@
-
+#!/bin/bash                                                                                        
 export DB_USER="db"
 export DB_PASS="db_pass"
+cd /home/will/thttpd_dev
 thttpd -C thttpd.conf& 
