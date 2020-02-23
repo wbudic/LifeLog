f160001989402d6d4f000c8faccb5f3b73b3af85
Will Budic <vilijanc@gmail.com>
2020-02-23 17:35:15 +1100

    new createLOGCATSREFStmt

--- htdocs/cgi-bin/system/modules/Settings.pm
+++ htdocs/cgi-bin/system/modules/Settings.pm
@@ -119,7 +119,15 @@ return qq(
     CREATE INDEX idx_auth_name_passw ON AUTH (ALIAS, PASSW);
 )}
 sub createNOTEStmt {return qq(CREATE TABLE NOTES (LID INTEGER PRIMARY KEY NOT NULL, DOC TEXT);)}
-sub createLOGCATSREFStmt {return qq(CREATE TABLE LOGCATSREF (LID INTEGER NOT NULL, CID TINY NOT NULL);)}
+sub createLOGCATSREFStmt {
+return qq(
+    CREATE TABLE LOGCATSREF (
+        LID INTEGER NOT NULL,
+        CID TINY NOT NULL,
+    FOREIGN KEY (LID) REFERENCES LOG(ID),
+    FOREIGN KEY(CID) REFERENCES CAT(ID)
+    );
+)}
 
 sub getConfiguration {
     my $db = shift;
