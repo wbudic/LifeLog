251708253b349991d4faadd5babfbbc7f26f5f5b
Will Budic <vilijanc@gmail.com>
2020-03-02 05:31:41 +1100

    OpenSSL requirments added.

--- install_modules.sh
+++ install_modules.sh
@@ -8,13 +8,14 @@
 # Uncoment the  following git pull if also wanting to perform a latest pull
 # from the master (upgrade) repository.
 ## git pull
-sudo cat Installation.txt | grep 'sudo cpan' > install_lifelog_modules_1.1.8.sh
+sudo cat Installation.txt | grep 'sudo apt install' > install_lifelog_modules_1.1.8.sh
+sudo cat Installation.txt | grep 'sudo cpan' >> install_lifelog_modules_1.1.8.sh
 sudo chmod +x install_lifelog_modules_1.1.8.sh
 # The following is actual installation file generated. Don't run outside this script.
 # You can delete or keep after running this script.
 sudo ./install_lifelog_modules_1.1.8.sh
 
-echo "\nDone with Life Log modules instalattion!\n"
+echo "\nDone with Life Log modules installation!\n"
 
 
 
