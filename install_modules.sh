6a0371eb2686667eec8faeb64a9ccacddfb51ad9
Will Budicm <vilijanac@gmail.com>
2020-05-26 20:52:26 +1000

    Updated, to use version variable.

--- install_modules.sh
+++ install_modules.sh
@@ -8,12 +8,14 @@
 # Uncoment the  following git pull if also wanting to perform a latest pull
 # from the master (upgrade) repository.
 ## git pull
-sudo cat Installation.txt | grep 'sudo apt install' > install_lifelog_modules_1.1.8.sh
-sudo cat Installation.txt | grep 'sudo cpan' >> install_lifelog_modules_1.1.8.sh
-sudo chmod +x install_lifelog_modules_1.1.8.sh
+VER="2.0"
+SCRIPT="install_lifelog_modules_$VER.sh"
+sudo cat Installation.txt | grep 'sudo apt install' > $SCRIPT
+sudo cat Installation.txt | grep 'sudo cpan' >> $SCRIPT
+sudo chmod +x $SCRIPT
 # The following is actual installation file generated. Don't run outside this script.
 # You can delete or keep after running this script.
-sudo ./install_lifelog_modules_1.1.8.sh
+sudo ./$SCRIPT
 
 echo "\nDone with Life Log modules installation!\n"
 
