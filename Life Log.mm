a69484086c20e27314aa8a789862d61aa8211888
wbudic <vilijanac@gmail.com>
2021-02-14 21:33:09 +1100

    gitignore updated

---
+++ Life Log.mm
@@ -0,0 +1,4 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Error while parsing file:/home/will/dev/LifeLog/README.md" FOLDED="false" ID="ID_1595600632" CREATED="1612332911147" MODIFIED="1612332911147"/>
+</map>
--- startDevWebServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="/home/will/dev/LifeLog"
-
-if [ ! -d "$DIR/log" ]; then
-    mkdir "$DIR/log"
-fi
-cd $DIR
-thttpd -C ./thttpd.conf &
